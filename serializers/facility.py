739b8805b22d093cd91f11ff2197e0a4b39cc35e
TARS <ritvikjain99@gmail.com>
2020-04-18 23:26:46 +0530

    Add patch for facility

--- serializers/facility.py
+++ serializers/facility.py
@@ -8,36 +8,36 @@ Hostel = swapper.load_model('kernel', 'Residence')
 
 
 class FacilitySerializer(serializers.ModelSerializer):
-    """
+    '''
     Serializer for Facility objects
-    """
+    '''
 
     timings = TimingSerializer(many=True,)
 
     class Meta:
-        """
+        '''
         Meta class for FacilitySerializer
-        """
+        '''
 
         model = Facility
         fields = [
-            "id",
-            "name",
-            "description",
-            "display_picture",
-            "timings",
+            'id',
+            'name',
+            'description',
+            'display_picture',
+            'timings',
         ]
 
     def create(self, validated_data):
-        timing_data = validated_data.pop("timings")
+        timing_data = validated_data.pop('timings')
         timing_serializer = TimingSerializer(data=timing_data, many=True)
         timing_serializer.is_valid(raise_exception=True)
 
-        hostel_code = self.context["hostel__code"]
+        hostel_code = self.context['hostel__code']
         try:
             hostel = Hostel.objects.get(code=hostel_code)
         except Exception:
-            raise serializers.ValidationError("Wrong hostel code")
+            raise serializers.ValidationError('Wrong hostel code')
 
         try:
             facility = Facility.objects.create(**validated_data, hostel=hostel,)
@@ -45,7 +45,17 @@ class FacilitySerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Wrong fields for facility.')
 
         timing_objects = timing_serializer.save()
-        for timing in timing_objects:
-            facility.timings.add(timing)
+        facility.timings.add(*timing_objects)
 
         return facility
+    
+    def update(self, instance, validated_data):
+        if 'timings' in validated_data.keys():
+            timings = validated_data.pop('timings')
+            timing_serializer = TimingSerializer(data=timings, many=True)
+            timing_serializer.is_valid(raise_exception=True)
+            timings = timing_serializer.save()
+            instance.timings.clear()
+            instance.timings.add(*timings)
+        return super().update(instance, validated_data)
+
--- views/facility.py
+++ views/facility.py
@@ -1,25 +1,22 @@
 import swapper
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from bhawan_app.models import Facility
 from bhawan_app.serializers.facility import FacilitySerializer
+from bhawan_app.pagination.custom_pagination import CustomPagination
+from bhawan_app.managers.services import is_hostel_admin
 
 
-class FacilityViewset(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class FacilityViewset(viewsets.ModelViewSet):
     """
     Detail view for getting facility information of a single hostel
     """
 
     serializer_class = FacilitySerializer
-    permission_classes = [
-        IsAuthenticated,
-    ]
+    permission_classes = [IsAuthenticated,]
+    pagination_class = CustomPagination
+    allowed_methods = ['GET', 'POST', 'PATCH']
 
     def get_queryset(self):
         """
@@ -27,12 +24,38 @@ class FacilityViewset(
         :return: the queryset of facilities of a hostel
         """
 
-        hostel = self.kwargs["hostel__code"]
+        hostel = self.kwargs['hostel__code']
         queryset = Facility.objects.filter(hostel__code=hostel)
 
         return queryset
 
     def get_serializer_context(self):
         return {
-            "hostel__code": self.kwargs["hostel__code"],
+            'hostel__code': self.kwargs['hostel__code'],
         }
+
+    def create(self, request, hostel__code):
+        """
+        Create facility instance if user has required permissions.
+        :return: new facility instance
+        """
+        if is_hostel_admin(request.person):
+            return super().create(request, hostel__code)
+            
+        return Response(
+            {"You are not allowed to perform this action !"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+    
+    def partial_update(self, request, hostel__code, pk=None):
+        """
+        Update facility instance if user has required permissions.
+        :return: updated facility instance
+        """
+        if is_hostel_admin(request.person):
+            return super().partial_update(request, hostel__code, pk)
+            
+        return Response(
+            {"You are not allowed to perform this action !"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
