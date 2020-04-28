8ea08b87d080fd5151a4d204e3173a0030cf8efd
TARS <ritvikjain99@gmail.com>
2020-04-28 17:40:37 +0530

    Add patch for Authorities

--- views/hostel_admin.py
+++ views/hostel_admin.py
@@ -1,16 +1,24 @@
-from rest_framework import viewsets
+import swapper
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from bhawan_app.models.roles.hostel_admin import HostelAdmin
 from bhawan_app.serializers.hostel_admin import HostelAdminSerializer
 from bhawan_app.constants import designations
+from bhawan_app.pagination.custom_pagination import CustomPagination
+from bhawan_app.managers.services import is_warden
+
+Person = swapper.load_model('Kernel', 'Person')
 
 class HostelAdminViewset(viewsets.ModelViewSet):
     """
-    List view for getting admin information of all hostels
+    CRUD views for getting admin information of all hostels
     """
 
     serializer_class = HostelAdminSerializer
-    allowed_methods = ['GET',]
+    allowed_methods = ['GET', 'POST', 'PATCH',]
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         """
@@ -46,3 +54,24 @@ class HostelAdminViewset(viewsets.ModelViewSet):
         filters['person__residentialinformation__residence__code']= \
                 self.kwargs['hostel__code']
         return filters
+
+    def partial_update(self, request, hostel__code, pk=None):
+        if is_warden(request.person):
+            instance = self.get_object()
+            serializer = self.get_serializer(
+                instance,
+                data=request.data,
+                partial=True,
+            )
+            serializer.is_valid(raise_exception=True)
+            if 'person' in request.data.keys():
+                person = Person.objects.get(pk=request.data['person'])
+                serializer.save(person=person)
+            else:
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(
+            'Only warden is allowed to perform this action !',
+            status=status.HTTP_403_FORBIDDEN,
+        )
